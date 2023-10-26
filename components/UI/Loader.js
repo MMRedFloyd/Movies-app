4e12c01bf1c3a6c710b49cab03aa7aefa00bc891
MMRedFloyd <mirza-m97@hotmail.com>
2023-10-26 23:51:07 +0200

    propagate loader

--- components/UI/Loader.js
+++ components/UI/Loader.js
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import PropagateLoader from "react-spinners/PropagateLoader";
 
 export default function Loader(props) {
   const loading = useSelector((state) => state.auth.loading);
+  const [isVisible, setIsVisible] = useState(true);
 
   const override = {
-    display: "block",
+    display: isVisible ? "block" : "none",
     borderColor: "#ad484a",
     position: "fixed",
     top: "50%",
@@ -13,11 +15,26 @@ export default function Loader(props) {
     transform: "translate(-50%, -50%)",
   };
 
+  useEffect(() => {
+    if (loading || props.loading) {
+      setIsVisible(true);
+
+      const timeout = setTimeout(() => {
+        setIsVisible(false);
+      }, 5000);
+      return () => {
+        clearTimeout(timeout);
+      };
+    } else {
+      setIsVisible(false);
+    }
+  }, [loading, props.loading]);
+
   return (
     <>
       <PropagateLoader
         color="#ad484a"
-        loading={loading || props.loading}
+        loading={isVisible}
         cssOverride={override}
       />
     </>
